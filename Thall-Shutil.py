902ad138f7ec03d4dfbf1f5eaf58dcddfae20390
Terri Hall <webdev.terrihall@gmail.com>
2017-12-30 18:53:12 -0700

    Add files via upload

---
+++ Thall-Shutil.py
@@ -0,0 +1,13 @@
+#!/user/bin/python
+# Python Ver:     2.7
+#
+# Author:	      Terri Hall
+#
+# Purpose:	       Create a program that will move .txt files from one folder to another with a click of a button.
+#
+# Tested OS:       This code was written and tested to work with Windows 10.
+ 
+
+import shutil
+import os
+shutil.move('C:/Users/webde/Desktop/FolderA', 'C:/Users/webde/Desktop/FolderB')
